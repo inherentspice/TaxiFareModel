714d5c109968fcca34aacf4cf13a0fce3f92f180
Stephen Smith <stephenlehanesmith@gmail.com>
2022-06-23 02:43:34 -0700

    completed predict file and added xgboost to test

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -11,14 +11,14 @@ from memoized_property import memoized_property
 from mlflow.tracking import MlflowClient
 import mlflow
 from joblib import dump
+from xgboost import XGBRegressor
+
 
 
 MLFLOW_URI = "https://mlflow.lewagon.ai/"
-EXPERIMENT_NAME = "[SH] [inherentspice] TaxiFareModel + version-1"
-
 
 class Trainer():
-    def __init__(self, X, y):
+    def __init__(self, X, y, **kwargs):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -26,7 +26,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
-        self.experiment_name = "[SH] [inherentspice] TaxiFareModel + version-1"
+        name = kwargs.get('experiment_name', 'version_1')
+        self.experiment_name = f'[SH] [inherentspice] TaxiFareModel + {name}'
 
     def set_pipeline(self, **kwargs):
         """defines the pipeline as a class attribute"""
@@ -77,7 +78,7 @@ class Trainer():
             (kwargs.get('model_name', 'linear_model'), kwargs.get('model', LinearRegression()))
     ])
         self.pipeline = pipe
-        self.mlflow_log_param("estimator", "LinearRegression")
+        self.mlflow_log_param("estimator", kwargs.get("model_name", "LinearRegression"))
         return self
 
     def run(self, **kwargs):
@@ -86,7 +87,7 @@ class Trainer():
         self.pipeline.fit(self.X, self.y)
         return self
 
-    def evaluate(self, X_test, y_test):
+    def evaluate(self, X_test, y_test, **kwargs):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
 
@@ -122,12 +123,14 @@ class Trainer():
 
 
 if __name__ == "__main__":
-    df = TaxiFareModel.data.get_data(nrows=1_000)
+    df = TaxiFareModel.data.get_data(nrows=100_000)
     df_clean = TaxiFareModel.data.clean_data(df)
     X = df_clean.drop(columns='fare_amount')
     y = df_clean['fare_amount']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    trainer = Trainer(X_train, y_train)
-    trainer.run()
-    trainer.save_model("test_model")
-    print(trainer.evaluate(X_test, y_test))
+    boost = XGBRegressor()
+    trainer = Trainer(X_train, y_train, experiment_name='experiment')
+    trainer.run(center=True, model_name='XGBoostRegressor', model=boost)
+    trainer.save_model("model")
+    score = trainer.evaluate(X_test, y_test)
+    print(score)
